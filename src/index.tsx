6b8644d6aa952d77015b90015c7c268993b3f3b0
waspryo <wasp2123321@gmail.com>
2019-09-28 15:25:34 +0900

    setup webpack babelrc

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 export default function App(): JSX.Element {
+    const sum = (a:number, b:number):number =>  a + b
+    
     return (
         <h1>
-           hello 
+        hello underworld!!{sum(15,15)}
         </h1>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+const path = require("path");
+const rules = [
+  {
+    test: /\.tsx?/,
+    exclude: /node_modules/,
+    loader: "babel-loader"
+  }
+];
+
 module.exports = {
-    target: 'web',
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: null,
-        filename: 'bundle.js'
-    }
-}
+  target: "web",
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path:__dirname + '/dist',
+    filename: "bundle.js"
+  },
+  module: { rules },
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
+  devServer: {
+    contentBase: "./",
+    port: 5000
+  }
+};
