56714d0358393d9f2d71a0878b9d727e436603d4
waspryo <wasp2123321@gmail.com>
2019-09-29 21:26:44 +0900

    add completeTodo function

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,55 @@
 import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
-type FromElem = React.FormEvent<HTMLFormElement>
+type FromElem = React.FormEvent<HTMLFormElement>;
 
 interface ITtodo {
-    text: string
-    complete: boolean
+  text: string;
+  complete: boolean;
 }
 export default function App(): JSX.Element {
-    const [value, setValue] = useState<string>('')
-    const [todos, setTodos] = useState<ITtodo[]>([])
+  const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITtodo[]>([]);
 
-    const handleSubmit = (e:FromElem):void => {
-        e.preventDefault()
-        addTodo(value)
-        setValue('')
-    }
+  const handleSubmit = (e: FromElem): void => {
+    e.preventDefault();
+    addTodo(value);
+    setValue("");
+  };
 
-    const addTodo = (text: string) => {
-        const newTodos: ITtodo[] = [...todos, {text, complete: false}]
-        setTodos(newTodos)
-    }
+  const addTodo = (text: string): void => {
+    const newTodos: ITtodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
 
-    console.log(todos)
+  const completeTodo = (index: number): void => {
+    const newTodos: ITtodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
   return (
     <Fragment>
       <h1>Todo List</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add Todo</button>
       </form>
       <section>
-          {todos.map((todo:ITtodo, index:number) => {
-             return <div key={index}>{todo.text}</div>
-          })}
+        {todos.map((todo: ITtodo, index: number) => (
+          <Fragment>
+            <div>{todo.text}</div>
+            <button type="button" onClick={() => completeTodo(index)}>
+              {" "}
+              {todo.complete ? "Incomplete" : "Complete"}{' '}
+            </button>
+          </Fragment>
+        ))}
       </section>
     </Fragment>
   );
